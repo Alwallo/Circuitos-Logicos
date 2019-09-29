c788671f5b6bbe8bbd6a54d6f1189154617571ad
Alwallo <48070805+Alwallo@users.noreply.github.com>
2019-09-29 16:45:34 -0600

    Estructuras de datos modificadas

--- estructuras_datos/LinkedList.java
+++ estructuras_datos/LinkedList.java
@@ -5,6 +5,8 @@
  */
 package estructuras_datos;
 
+import javax.swing.JComponent;
+
 /**
  *
  * @author alvgo
@@ -12,22 +14,51 @@ package estructuras_datos;
 public class LinkedList {
     private Node head;
     private int size;
+    
+    /**
+     * Se crea el constructor de la clase, inicializando los atributos
+     */
     public LinkedList() {
         this.head = null;
         this.size = 0;
     }
+    
+    /**
+     * Metodo para verificar si la lista esta vacia
+     * @return Retorna true si la lista esta vacia, false si no
+     */
     public boolean isEmpty() {
         return this.head == null;
     }
+    
+    /**
+     * Metodo para conocer la longitud de la lista
+     * @return Retorna la longitud de la lista
+     */
     public int size() {
         return this.size;
     }
-    public void insertFirst(Object data) {
-        Node newBNode = new Node(data);
+    
+    /**
+     * Metodo para agregar nodos a la lista
+     * @param nombre Nombre del componente con el que sera referenciado
+     * @param posX Posicion en X del componente
+     * @param posY Posicion en Y del componente
+     * @param W Ancho del componente
+     * @param H Largo del componente
+     * @param comp Tipo de componente
+     */
+    public void insertFirst(String nombre, int posX, int posY, int W, int H, JComponent comp) {
+        Node newBNode = new Node(nombre, posX, posY, W, H, comp);
         newBNode.next = this.head;
         this.head = newBNode;
         this.size++;
     }
+    
+    /**
+     * Metodo para borrar el primer nodo de la lista
+     * @return Retorna el nodo borrado
+     */
     public Node deleteFirst() {
         if (this.head != null) {
             Node temp = this.head;
@@ -38,30 +69,42 @@ public class LinkedList {
             return null;
         }
     }
+    
     public void displayList() {
         Node current = this.head;
         while (current != null) {
-            System.out.println(current.getData());
             current = current.getNext();
         }
     }
-    public String find(String searchValue) {
+    
+    /**
+     * Metodo para buscar un componente
+     * @param searchValue Nombre del componente que sera buscado
+     * @return Retorna el nodo en busqueda si se encuentra
+     */
+    public Node find(String searchValue) {
         Node current = this.head;
         while (current != null) {
-            if (current.getData().equals(searchValue)) {
-                return current.toString();
+            if (current.getNombre().equals(searchValue)) {
+                return current;
             } else {
                 current = current.getNext();
             }
         }
         return null;
     }
+    
+    /**
+     * Metodo para borrar un nodo de la lista especifico
+     * @param searchValue Nombre del componente que sera borrado
+     * @return Retorna el nodo borrado
+     */
     public Node delete(String searchValue) {
         Node current = this.head;
         Node previous = this.head;
 
         while (current != null) {
-            if (current.getData().equals(searchValue)) {
+            if (current.getNombre().equals(searchValue)) {
                 if (current == this.head) {
                     this.head = this.head.getNext();
                 } else {
@@ -74,5 +117,13 @@ public class LinkedList {
             }
         }
         return null;
+    }
+    
+    /**
+     * Metodo para obtener el primer nodo de la lista
+     * @return Retorna el primer nodo
+     */
+    public Node getHead(){
+        return this.head;
     }
 }
--- estructuras_datos/Node.java
+++ estructuras_datos/Node.java
@@ -5,30 +5,145 @@
  */
 package estructuras_datos;
 
+import javax.swing.JComponent;
+
 /**
  *
  * @author alvgo
  */
 public class Node {
-    private Object data;
+    private String nombre;
+    private int posX, posY, W, H;
+    private JComponent comp;
     public Node next;
 
-    public Node(Object data) {
+    /**
+     * Constructor de la clase
+     * @param nombre Nombre del componente con el que sera referenciado
+     * @param posX Posicion en X del componente
+     * @param posY Posicion en Y del componente
+     * @param W Ancho del componente
+     * @param H Largo del componente
+     * @param comp Tipo de componente
+     */
+    public Node(String nombre, int posX, int posY, int W, int H, JComponent comp) {
+        this.nombre = nombre;
+        this.posX = posX;
+        this.posY = posY;
+        this.W = W;
+        this.H = H;
+        this.comp = comp;
         this.next = null;
-        this.data = data;
     }
 
-    public Object getData() {
-        return this.data;
+    /**
+     * Metodo para obtener el nombre del componente guardado en un nodo especifico
+     * @return Retorna el nombre del componente
+     */
+    public String getNombre() {
+        return nombre;
     }
 
-    public void setData(Object data) {
-        this.data = data;
+    /**
+     * Metodo para insertar el nombre a algun componente de la lista
+     * @param nombre Nombre que sera insertado
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
+
+    /**
+     * Metodo para obtener la posicion en X del componente guardado en un nodo especifico
+     * @return Retorna la posicion en X del componente
+     */
+    public int getPosX() {
+        return posX;
+    }
+
+    /**
+     * Metodo para insertar la posicion en X a algun componente de la lista
+     * @param posX Posicion en X que sera insertado
+     */
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+    
+    /**
+     * Metodo para obtener la posicion en Y del componente guardado en un nodo especifico
+     * @return Retorna la posicion en Y del componente
+     */
+    public int getPosY() {
+        return posY;
+    }
+
+    /**
+     * Metodo para insertar la posicion en Y a algun componente de la lista
+     * @param posY Posicion en Y que sera insertado
+     */
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    /**
+     * Metodo para obtener el ancho del componente guardado en un nodo especifico
+     * @return Retorna el ancho del componente
+     */
+    public int getW() {
+        return W;
+    }
+
+    /**
+     * Metodo para insertar el ancho a algun componente de la lista
+     * @param W Ancho que sera insertado
+     */
+    public void setW(int W) {
+        this.W = W;
+    }
+
+    /**
+     * Metodo para obtener el largo del componente guardado en un nodo especifico
+     * @return Retorna el largo del componente
+     */
+    public int getH() {
+        return H;
+    }
+
+    /**
+     * Metodo para insertar el largo a algun componente de la lista
+     * @param H Largo que sera insertado
+     */
+    public void setH(int H) {
+        this.H = H;
+    }
+
+    /**
+     * Metodo para obtener el componente guardado en un nodo especifico
+     * @return Retorna el componente
+     */
+    public JComponent getComp() {
+        return comp;
+    }
+
+    /**
+     * Metodo para insertar el componente de la lista
+     * @param comp Componente que sera insertado
+     */
+    public void setComp(JComponent comp) {
+        this.comp = comp;
+    }
+    
+    /**
+     * Metodo para obtener el nodo siguiente del nodo actual
+     * @return Retorna el siguiente del nodo actual
+     */
     public Node getNext() {
         return this.next;
     }
 
+    /**
+     * Metodo para insertar el siguiente del nodo actual
+     * @param BNode Nodo siguiente al actual
+     */
     public void setNext(Node BNode) {
         this.next = BNode;
     }
